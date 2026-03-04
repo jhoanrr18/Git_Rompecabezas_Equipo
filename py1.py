eb4676ec2d40f47e8fbac16dbce23a143948b40c
camipenagos777-ui <camipenagos777@gmail.com>
2026-03-04 13:01:32 -0500

    ivan

---
+++ py1.py
@@ -0,0 +1,21 @@
+i = 0
+while i == 0:
+    print("==== MENU DE OPCIONES ====")
+    print("1. Cevhiche")
+    print("2. Aji de Gallina")
+    print("3. Lomo Salteado")
+    print("4. Causa Rellena")
+    print("5. Salir")
+    op = int(input("Ingrese una opcion: "))
+    
+    if op==1:
+        print("Aqui tiene su cevichote:")
+    elif op==2:
+        print("Aqui tiene su aji de Gallina")
+    elif op==3:
+        print("Aqui tiene su lomo salteado")
+    elif op==4:
+        print("Aqui tiene su causa rellena")
+    elif op==5:
+        print("Gracias por su visita ¡vuelva pronto!")
+     
